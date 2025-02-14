b91083522db22d3dd8c67ef7a658951ebfded5e3
olshum <olshum8@gmail.com>
2025-02-14 16:19:56 +0200

    valentynka

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.scss';
 
-export function App() {
-  const [count, setCount] = useState(0)
+export const App: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bgoverlay">
+      <div className={'container' + (isOpen ? ' open' : '')}>
+        <span className="ico">
+          <span className="ico2"></span>
+          <span className="title" onClick={() => setIsOpen(true)}>
+            Click Me
+          </span>
+        </span>
+        {isOpen && (
+          <div className="endtext">
+            <span
+              className="close"
+              title="Restart"
+              onClick={() => setIsOpen(false)}
+            >
+              <i className="fa fa-times"></i>
+            </span>
+            <h1>Dear Pyrizhok iz Vyshnyamy</h1>
+            <h2>Will you be my valentine forever?</h2>
+            <img src="/images/mouse.png" alt="mouse" />
+            <audio autoPlay>
+              <source src="/audio/love_you.mp3" type="audio/mpeg" />
+            </audio>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </div>
+  );
+};
